841f923e2e4caabb383db2fbb1178663b9cba013
mhamza24 <muhammadhamzakhalid24@gmail.com>
2024-10-10 11:53:34 +0500

    leetcode Q34 solution added

--- src/Practice/Solution.java
+++ src/Practice/Solution.java
@@ -7,13 +7,56 @@ public class Solution
 {
     public static void main(String[] args) {
         int[] arr={5,7,7,8,8,10};
+//        int[] ans=searchRange(arr,7);
+//        System.out.println(Arrays.toString(ans));
+
+
 
     }
 
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans={-1,-1};
+        int first=binarySearch(nums,target,true);
+        if(first==-1)
+        {
+            return  ans;
+        }
+        int last=binarySearch(nums,target,false);
+
+        return new int[] {first,last};
+    }
 
 
+     public int binarySearch(int[] arr,int target,boolean searchingFirstIndex){
+        int ans=-1,start=0,end=arr.length-1,mid=0;
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            if(arr[mid]==target)
+            {
+
+                ans=mid;
+                if(searchingFirstIndex)
+                {
+                    end=mid-1;
+                }else {
+                    start=mid+1;
+                }
 
 
+            }
+            if(arr[mid]>target)
+            {
+                end=mid-1;
+
+            } else if (arr[mid]<target) {
+                start=mid+1;
+            }
+
+        }
+
+        return ans;
+    }
 
 
 
