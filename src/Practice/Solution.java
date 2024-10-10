a80a6144d0784261370429a4dc750d36e1f42296
mhamza24 <muhammadhamzakhalid24@gmail.com>
2024-10-10 12:53:17 +0500

    Searching in infinite array

--- src/Practice/Solution.java
+++ src/Practice/Solution.java
@@ -6,57 +6,53 @@ import java.util.Arrays;
 public class Solution
 {
     public static void main(String[] args) {
-        int[] arr={5,7,7,8,8,10};
-//        int[] ans=searchRange(arr,7);
-//        System.out.println(Arrays.toString(ans));
-
-
+        int[] arr={3,5,7,9,10,90,100,130,140,160,170};
+        int ans=searchTarget(arr,80);
+        System.out.println(ans);
 
     }
 
-    public int[] searchRange(int[] nums, int target) {
-        int[] ans={-1,-1};
-        int first=binarySearch(nums,target,true);
-        if(first==-1)
+
+    static public int searchTarget(int[]  arr,int target)
+    {
+        int start=0,end=1;
+        while (target>arr[end])
         {
-            return  ans;
+            int newStart=end+1;
+            end=end+((end-start+1)*2);
+            start=newStart;
         }
-        int last=binarySearch(nums,target,false);
 
-        return new int[] {first,last};
+        return binarySearch(arr,target,start,end);
     }
 
-
-     public int binarySearch(int[] arr,int target,boolean searchingFirstIndex){
-        int ans=-1,start=0,end=arr.length-1,mid=0;
-        while(start<=end)
+    static public int binarySearch(int[] arr,int target,int start,int end)
+    {
+        while (start<=end)
         {
-            mid=start+(end-start)/2;
+            int mid=start+(end-start)/2;
             if(arr[mid]==target)
             {
-
-                ans=mid;
-                if(searchingFirstIndex)
-                {
-                    end=mid-1;
-                }else {
-                    start=mid+1;
-                }
-
-
+                return mid;
             }
             if(arr[mid]>target)
             {
                 end=mid-1;
-
-            } else if (arr[mid]<target) {
+            }
+            else if(arr[mid]<target)
+            {
                 start=mid+1;
             }
 
         }
-
-        return ans;
+        return -1;
     }
+
+
+
+
+
+
 
 
 
