2457b00b81c5ae177185112e21385dc098682c60
mhamza24 <muhammadhamzakhalid24@gmail.com>
2024-10-10 16:49:03 +0500

    leetcode Q1095 solved

--- src/Practice/Solution.java
+++ src/Practice/Solution.java
@@ -6,48 +6,73 @@ import java.util.Arrays;
 public class Solution
 {
     public static void main(String[] args) {
-        int[] arr={2,3,5,8,10,9,4,0};
-        System.out.println(binarySearch(arr,0,0,arr.length-1));
-       // int[] arr={2,3,5,8,10,9,4,0};
-       // System.out.println(ans);
-
+        int[] arr={1,2,3,4,5,3,1,6};
+        System.out.println(findElementWith(arr,8));
     }
 
-    public int peakIndexInMountainArray(int[] nums) {
-        int start=0,end=nums.length-1;
-        while (start<end)
-        {
-            int mid=start+(end-start)/2;
-            if(nums[mid+1]>nums[mid])
-            {
-                start=mid+1;
-            }
-            else if(nums[mid+1]<nums[mid])
-            {
-                end=mid;
-            }
-
-        }
-        return end;
-    }
-
-    static public int binarySearch(int[] arr,int target,int start,int end)
+    public static int findElementWith(int[] arr,int target)
     {
+        int peek=findMountainPeek(arr);
+        int leftside=findElementBinarySearch(arr,target,0,peek);
+        if(leftside!=-1)
+        {
+            return leftside;
+        }
+        return findElementBinarySearch(arr,target,peek,arr.length-1);
+    }
+    public  static int findMountainPeek(int[] arr)
+    {
+        int start=0,end= arr.length-1;
         while (start<end)
         {
             int mid=start+(end-start)/2;
-            if(arr[mid+1]>arr[mid])
-            {
-                start=mid+1;
-            }
-            else if(arr[mid+1]<arr[mid])
+            if(arr[mid]>arr[mid+1])
             {
                 end=mid;
             }
+            else if(arr[mid+1]>arr[mid])
+            {
+                start=mid+1;
+            }
 
         }
-        return end;
+        return start;
     }
+
+    public  static int findElementBinarySearch(int[] arr,int target,int start,int end)
+    {
+        boolean isAscending=arr[start]<arr[end];
+        while (start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if (arr[mid]==target)
+            {
+                return mid;
+            }
+            if(isAscending)
+            {
+                if(arr[mid]>target)
+                {
+                    end=mid-1;
+                }
+                else if(arr[mid]<target)
+                {
+                    start=mid+1;
+                }
+            }else {
+                if(arr[mid]>target)
+                {
+                    start=mid+1;
+                }
+                else if(arr[mid]<target)
+                {
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
 
 
 
