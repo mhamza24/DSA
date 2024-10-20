1277b0a6938e81d5e2a3330e49171855c33fdb8d
mhamza24 <muhammadhamzakhalid24@gmail.com>
2024-10-20 22:35:47 +0500

    leetcode Q 832 solved

--- src/binary/FlippingAnImage.java
+++ src/binary/FlippingAnImage.java
@@ -1,5 +1,25 @@
 package binary;
 
+import java.util.Arrays;
+
+//Google
 public class FlippingAnImage {
-    
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 1, 0 }, { 1, 0, 1 }, { 0, 0, 0 } };
+        System.out.println(arr.length);
+        System.out.println(Arrays.toString(flipAndInvertImage(arr)));
+    }
+
+    public static int[][] flipAndInvertImage(int[][] image) {
+
+        for (int[] row : image) {
+            for (int i = 0; i < (image[0].length + 1) / 2; i++) {
+                int tmep = row[i] ^ 1;
+                row[i] = row[image[0].length - 1 - i] ^ 1;
+                row[image[0].length - 1 - i] = tmep;
+            }
+        }
+
+        return image;
+    }
 }
